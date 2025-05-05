1d08d199581a8d321f6e970dbd5d1f0f76005855
Melkizedeki <mmagota2@gmail.com>
2025-05-05 16:25:40 +0300

    The commit after creating the model for expenses

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:expenses_tracker/pages/expenses.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Expenses(),
+    );
+  }
+}
