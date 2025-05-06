00f68acd86d1838590270e9abd9e04e0a1553e24
Melkizedeki <mmagota2@gmail.com>
2025-05-06 10:47:19 +0300

    The commit after accomplishing datetime picker

--- lib/components/inputing_expense_widget.dart
+++ lib/components/inputing_expense_widget.dart
@@ -1,3 +1,4 @@
+import 'package:expenses_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,12 +13,32 @@ class AddingExpenses extends StatefulWidget {
 class _AddingExpensesState extends State<AddingExpenses> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? theDatePicked;
 
   @override
   void dispose() {
     _titleController.dispose();
     _amountController.dispose();
     super.dispose();
+  }
+
+  void _showDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(
+      now.year - 1,
+      now.month,
+      now.day,
+    );
+
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      theDatePicked = pickedDate;
+    });
   }
 
   @override
@@ -36,28 +57,67 @@ class _AddingExpensesState extends State<AddingExpenses> {
               label: Text("Expenses"),
             ),
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            controller: _amountController,
-            decoration: InputDecoration(
-              hintText: "Enter the amount",
-              prefixText: "\$",
-              label: Text("Amount"),
-            ),
+          Row(
+            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  controller: _amountController,
+                  decoration: InputDecoration(
+                    hintText: "Enter the amount",
+                    prefixText: "\$",
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      theDatePicked == null
+                          ? "No date selected"
+                          : formatter.format(theDatePicked!),
+                    ),
+                    IconButton(
+                      onPressed: _showDatePicker,
+                      icon: Icon(
+                        Icons.calendar_month,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {
-              print(
-                _titleController.text,
-              );
-              print(
-                _amountController.text,
-              );
-            },
-            child: Text(
-              "Add Expenses",
-            ),
-          ),
+          Spacer(),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(
+                    _titleController.text,
+                  );
+                  print(
+                    _amountController.text,
+                  );
+                },
+                child: Text(
+                  "Add Expenses",
+                ),
+              ),
+              Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel"),
+              ),
+            ],
+          )
         ],
       ),
     );
